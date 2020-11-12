fd4481ff3d13d305d1b531c7a64fd0e5f15e62d6
Javi <javiercasaubon@gmail.com>
2020-11-12 04:18:16 -0300

    Clean up code

---
+++ app/src/main/java/com/test/exoplayer2/BlockCipherEncryptedDataSource.kt
@@ -0,0 +1,211 @@
+package com.test.exoplayer2
+
+import android.net.Uri
+import android.util.Log
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DataSpec
+import com.google.android.exoplayer2.upstream.TransferListener
+import com.test.exoplayer2.MainActivity.Companion.AES_TRANSFORMATION
+import java.io.EOFException
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.lang.RuntimeException
+import javax.crypto.Cipher
+import javax.crypto.CipherInputStream
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+const val TAG = "ENCRYPTING PROCESS"
+
+class BlockCipherEncryptedDataSource(
+    private val secretKeySpec: SecretKeySpec,
+    private val uri: Uri,
+    cipherTransformation: String = AES_TRANSFORMATION
+) : DataSource {
+    private val cipher: Cipher = Cipher.getInstance(cipherTransformation)
+    private lateinit var streamingCipherInputStream: StreamingCipherInputStream
+    private var bytesRemaining: Long = 0
+    private var isOpen = false
+    private val transferListeners = mutableListOf<TransferListener>()
+    private var dataSpec: DataSpec? = null
+
+    @Throws(EncryptedFileDataSourceException::class)
+    override fun open(dataSpec: DataSpec): Long {
+        this.dataSpec = dataSpec
+
+        if (isOpen) return bytesRemaining
+
+        try {
+            setupInputStream()
+            streamingCipherInputStream.forceSkip(dataSpec.position)
+            computeBytesRemaining(dataSpec)
+        } catch (e: IOException) {
+            throw EncryptedFileDataSourceException(e)
+        }
+
+        isOpen = true
+        transferListeners.forEach { it.onTransferStart(this, dataSpec, false) }
+
+        return C.LENGTH_UNSET.toLong()
+    }
+
+    private fun setupInputStream() {
+        val path = uri.path ?: throw RuntimeException("Tried decrypting uri with no path: $uri")
+        val encryptedFileStream = File(path).inputStream()
+        val initializationVector = ByteArray(cipher.blockSize)
+        encryptedFileStream.read(initializationVector)
+        streamingCipherInputStream =
+            StreamingCipherInputStream(
+                encryptedFileStream,
+                cipher,
+                IvParameterSpec(initializationVector),
+                secretKeySpec
+            )
+    }
+
+    @Throws(IOException::class)
+    private fun computeBytesRemaining(dataSpec: DataSpec) {
+        if (dataSpec.length != C.LENGTH_UNSET.toLong()) {
+            bytesRemaining = dataSpec.length
+            return
+        }
+
+        if (bytesRemaining == Int.MAX_VALUE.toLong()) {
+            bytesRemaining = C.LENGTH_UNSET.toLong()
+            return
+        }
+
+        bytesRemaining = streamingCipherInputStream.available().toLong()
+    }
+
+    @Throws(EncryptedFileDataSourceException::class)
+    override fun read(buffer: ByteArray, offset: Int, readLength: Int): Int {
+        if (bytesRemaining == 0L) {
+            Log.e(TAG, "End - No bytes remaining")
+            return C.RESULT_END_OF_INPUT
+        }
+
+        val bytesRead = try {
+            streamingCipherInputStream.read(buffer, offset, readLength)
+        } catch (e: IOException) {
+            throw EncryptedFileDataSourceException(e)
+        }
+
+        // Reading -1 means an error occurred
+        if (bytesRead < 0) {
+            if (bytesRemaining != C.LENGTH_UNSET.toLong())
+                throw EncryptedFileDataSourceException(EOFException())
+            return C.RESULT_END_OF_INPUT
+        }
+
+        // Bytes remaining will be unset if file is too large for an int
+        if (bytesRemaining != C.LENGTH_UNSET.toLong())
+            bytesRemaining -= bytesRead.toLong()
+
+        dataSpec?.let { nonNullDataSpec ->
+            transferListeners.forEach {
+                it.onBytesTransferred(this, nonNullDataSpec, false, bytesRead)
+            }
+        }
+        return bytesRead
+    }
+
+    override fun addTransferListener(transferListener: TransferListener) {
+        transferListeners.add(transferListener)
+    }
+
+    override fun getUri(): Uri = uri
+
+    @Throws(EncryptedFileDataSourceException::class)
+    override fun close() {
+        Log.e(TAG, "Closing stream")
+        try {
+            streamingCipherInputStream.close()
+        } catch (e: IOException) {
+            throw EncryptedFileDataSourceException(e)
+        } finally {
+            if (isOpen) {
+                isOpen = false
+                dataSpec?.let { nonNullDataSpec ->
+                    transferListeners.forEach { it.onTransferEnd(this, nonNullDataSpec, false) }
+                }
+            }
+        }
+    }
+
+    class EncryptedFileDataSourceException(cause: IOException?) : IOException(cause)
+    class StreamingCipherInputStream(
+        private val sourceStream: InputStream,
+        private var cipher: Cipher,
+        private val initialIvParameterSpec: IvParameterSpec,
+        private val secretKeySpec: SecretKeySpec
+    ) : CipherInputStream(
+        sourceStream, cipher
+    ) {
+        private val cipherBlockSize: Int = cipher.blockSize
+
+        @Throws(IOException::class)
+        override fun read(b: ByteArray, off: Int, len: Int): Int = super.read(b, off, len)
+
+        fun forceSkip(bytesToSkip: Long) {
+            val bytesSinceStartOfCurrentBlock = bytesToSkip % cipherBlockSize
+
+            val bytesUntilPreviousBlockStart =
+                bytesToSkip - bytesSinceStartOfCurrentBlock - cipherBlockSize
+
+            try {
+                if (bytesUntilPreviousBlockStart <= 0) {
+                    cipher.init(
+                        Cipher.DECRYPT_MODE,
+                        secretKeySpec,
+                        initialIvParameterSpec
+                    )
+                    return
+                }
+
+                var skipped = sourceStream.skip(bytesUntilPreviousBlockStart)
+                while (skipped < bytesUntilPreviousBlockStart) {
+                    sourceStream.read()
+                    skipped++
+                }
+
+                val previousEncryptedBlock = ByteArray(cipherBlockSize)
+
+                sourceStream.read(previousEncryptedBlock)
+
+                cipher.init(
+                    Cipher.DECRYPT_MODE,
+                    secretKeySpec,
+                    IvParameterSpec(previousEncryptedBlock)
+                )
+                skip(bytesUntilPreviousBlockStart + cipherBlockSize)
+
+                val discardableByteArray = ByteArray(bytesSinceStartOfCurrentBlock.toInt())
+                read(discardableByteArray)
+            } catch (e: Exception) {
+                Log.e(TAG, "Encrypted video skipping error", e)
+                throw e
+            }
+        }
+
+        // We need to return the available bytes from the upstream.
+        // In this implementation we're front loading it, but it's possible the value might change during the lifetime
+        // of this instance, and reference to the stream should be retained and queried for available bytes instead
+        @Throws(IOException::class)
+        override fun available(): Int {
+            return sourceStream.available()
+        }
+    }
+}
+
+class BlockCipherEncryptedDataSourceFactory(
+    private val secretKeySpec: SecretKeySpec,
+    private val uri: Uri,
+    private val cipherTransformation: String = AES_TRANSFORMATION
+) : DataSource.Factory {
+    override fun createDataSource(): BlockCipherEncryptedDataSource {
+        return BlockCipherEncryptedDataSource(secretKeySpec, uri, cipherTransformation)
+    }
+}
--- app/src/main/java/com/test/exoplayer2/MainActivity.kt
+++ app/src/main/java/com/test/exoplayer2/MainActivity.kt
@@ -1,85 +1,58 @@
 package com.test.exoplayer2
 
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.extractor.ExtractorsFactory
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import kotlinx.coroutines.launch
 import java.io.File
-import java.util.jar.Manifest
 import javax.crypto.Cipher
+import javax.crypto.CipherOutputStream
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 class MainActivity : AppCompatActivity() {
     private var mCipher: Cipher? = null
-    private var mSecretKeySpec: SecretKeySpec? = null
-    private var mIvParameterSpec: IvParameterSpec? = null
-    private var mEncryptedFile: File? = null
-    private var mSimpleExoPlayerView: PlayerView? = null
+    private val secretKeySpec: SecretKeySpec get() = SecretKeySpec(secretKey, AES_ALGORITHM)
+    private val ivParameterSpec: IvParameterSpec? get() = IvParameterSpec(initialIv)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mSimpleExoPlayerView = findViewById(R.id.simpleexoplayerview)
-        mEncryptedFile = File(filesDir, ENCRYPTED_FILE_NAME)
-        mSecretKeySpec = SecretKeySpec(secretKey, AES_ALGORITHM)
-        mIvParameterSpec = IvParameterSpec(initialIv)
         try {
             mCipher = Cipher.getInstance(AES_TRANSFORMATION)
-            mCipher!!.init(Cipher.DECRYPT_MODE, mSecretKeySpec, mIvParameterSpec)
+            mCipher!!.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec)
         } catch (e: Exception) {
             e.printStackTrace()
         }
-    }
-
-    private fun hasFile(): Boolean {
-        return (mEncryptedFile != null && mEncryptedFile!!.exists()
-            && mEncryptedFile!!.length() > 0)
     }
 
     fun encryptVideo(view: View) {
-        if (hasFile()) {
-            Log.d(javaClass.canonicalName, "encrypted file found, no need to recreate")
-            return
-        }
-        try {
+        lifecycleScope.launch {
             val encryptionCipher = Cipher.getInstance(AES_TRANSFORMATION)
-            encryptionCipher.init(Cipher.ENCRYPT_MODE, mSecretKeySpec, mIvParameterSpec)
-            DownloadAndEncryptFileTask(
-                "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-                // "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4",
-                mEncryptedFile,
-                encryptionCipher
-            ).execute()
-        } catch (e: Exception) {
-            e.printStackTrace()
+            encryptionCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec)
+            encryptFile(encryptionCipher)
         }
     }
 
     fun playVideo(view: View) {
-        val bandwidthMeter = DefaultBandwidthMeter.Builder(view.context).build()
-
         val player = SimpleExoPlayer.Builder(view.context).build()
-        mSimpleExoPlayerView!!.player = player
+        findViewById<PlayerView>(R.id.simpleexoplayerview).player = player
 
-        val dataSourceFactory: DataSource.Factory = EncryptedFileDataSourceFactory(
-            mCipher!!,
-            mSecretKeySpec!!,
-            bandwidthMeter
+        val uri = Uri.fromFile(encryptedFile)
+        val dataSourceFactory: DataSource.Factory = BlockCipherEncryptedDataSourceFactory(
+            secretKeySpec,
+            uri,
+            AES_TRANSFORMATION
         )
         try {
-            val uri = Uri.fromFile(mEncryptedFile)
             val videoSource =
                 ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
                     MediaItem.Builder().setUri(uri).build()
@@ -96,43 +69,28 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val AES_ALGORITHM = "AES"
         const val AES_TRANSFORMATION = "AES/CBC/PKCS7Padding"
-        private const val ENCRYPTED_FILE_NAME = "encrypted.mp4"
 
-        val secretKey = byteArrayOf(
-            'c'.toByte(),
-            'o'.toByte(),
-            'd'.toByte(),
-            'i'.toByte(),
-            'n'.toByte(),
-            'g'.toByte(),
-            'a'.toByte(),
-            'f'.toByte(),
-            'f'.toByte(),
-            'a'.toByte(),
-            'i'.toByte(),
-            'r'.toByte(),
-            's'.toByte(),
-            'c'.toByte(),
-            'o'.toByte(),
-            'm'.toByte()
-        )
-        var initialIv = byteArrayOf(
-            'l'.toByte(),
-            'o'.toByte(),
-            'd'.toByte(),
-            'i'.toByte(),
-            'n'.toByte(),
-            'g'.toByte(),
-            'a'.toByte(),
-            'f'.toByte(),
-            'f'.toByte(),
-            'a'.toByte(),
-            'i'.toByte(),
-            'r'.toByte(),
-            's'.toByte(),
-            'c'.toByte(),
-            'o'.toByte(),
-            'f'.toByte()
-        )
+        val secretKey = "85BE62F9AC34D107".map { it.toByte() }.toByteArray()
+        var initialIv = "1234567890ABCDEF".map { it.toByte() }.toByteArray()
+    }
+
+    private val encryptedFile get() = File(filesDir, "encrypted.mp4")
+
+    private fun encryptFile(cipher: Cipher) {
+        val unencryptedFile = File(filesDir, "unencrypted.mp4")
+        encryptedFile.delete()
+
+        unencryptedFile.inputStream().use { unencryptedFileInputStream ->
+            encryptedFile.outputStream().use { fileOutputStream ->
+                fileOutputStream.write(initialIv)
+
+                CipherOutputStream(fileOutputStream, cipher).use { cipherOutputStream ->
+                    unencryptedFileInputStream.copyTo(cipherOutputStream)
+                }
+            }
+        }
+
+        Log.d(TAG, "File encrypted successfully.")
     }
 }
+
